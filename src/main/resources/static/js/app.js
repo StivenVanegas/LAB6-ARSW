b6ad0ec7b9b69549189978d1f3050800e7f3eec6
Fabian Ramirez <fabian.ramirez-p@mail.escuelaing.edu.co>
2021-03-09 23:47:17 -0500

    imag y parte final

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -30,15 +30,48 @@ var Controlador = (function(){
         var fila= "<tr><th>Blueprints name</th><th>Number of points</th><th>Open</th></tr>";
          $("#tabla").append(fila);
         for(var[clave, valor] of dataMap){
-            fila = "<tr> <td>"+ clave + "</td> <td>"+valor+"</td> </tr>";
+            fila = "<tr> <td>"+ clave + "</td><td>"+valor+"</td><td><input type ='button' value='OPEN' onclick='Controlador.getBluePrintsByAuthor(this)' id ='"+clave+"'><td> </tr>";
             $("#tabla").append(fila);
         }
         $("#lblPoints").text("Total user points: "+TotalPoints);   
     };
-    
-    return{
-        getBlueprints: getBlueprints
+
+    var getBluePrintsByAuthor=function(aqui){
+    var id = aqui.id;
+    var authorName = document.getElementById("authorName").value;
+            var authorUrl = "http://localhost:8080/blueprints/" + authorName + "/"+ id;
+            fetch(authorUrl)
+                    .then(response => response.json())
+                    .then(json => Dibujar(json))
+                    .catch(err => {
+                       console.log(err);
+                    });
+
+    console.log(id);
+
     };
+
+    var Dibujar= function(res){
+    var points = res.points;
+    var canvas = document.getElementById("myCanvas");
+        if(canvas.getContext){
+        var ctx = canvas.getContext("2d");
+        for(var i in points){
+            if (i===0){
+                ctx.moveTo(points[i].x,points[i].y);
+            }
+            else {
+            ctx.lineTo(points[i].x,points[i].y);
+            ctx.stroke();
+            }
+        }
+       }
+    };
+    return{
+        getBlueprints: getBlueprints,
+        getBluePrintsByAuthor:getBluePrintsByAuthor
+    };
+
     
 })();
 
